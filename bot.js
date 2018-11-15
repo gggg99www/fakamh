bc720ec69c4632b0286439c59409c3016e331ece
gggg99www <44984945+gggg99www@users.noreply.github.com>
2018-11-15 23:53:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- 
+const prefix = 'fff';
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
    client.user.setActivity("اتضن انك قد طمست هويتي ؟ بالمشمش ^_^",{type: 'WATCHING'})
